4d6a0e5a792cccc2682131c2c27315761d5ac73e
sonnyt <sonny@stylecaster.com>
2013-04-23 13:54:51 -0400

    Javascripts now user site config usernames and mobile friendly meta added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
     // Initiate twitter
     $('.tweet').twittie({
-        username: 'sonnyt',
+        username: twitter_username,
         count: 1,
         hideReplies: true
     });
 
     // Get Themeforest count
-    $.getJSON('http://marketplace.envato.com/api/edge/user-items-by-site:sonnyt.json', function (data) {
+    $.getJSON('http://marketplace.envato.com/api/edge/user-items-by-site:'+ themeforest_username +'.json', function (data) {
         var count = data['user-items-by-site'][0].items;
 
         $('#themeforest em').html(count);
     });
 
     // Get GitHub repo count
-    $.getJSON('https://api.github.com/users/sonnyt/repos?callback=?', function (data) {
+    $.getJSON('https://api.github.com/users/'+ github_username +'/repos?callback=?', function (data) {
         var count = data['data'].length;
 
         $('#github em').html(count);
