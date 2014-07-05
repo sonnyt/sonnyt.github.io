1701769cc86b5cc3af543bdc7682fc02fc1d2790
Sonny <mail@sonnyt.com>
2014-07-05 00:16:24 -0400

    Removed the wheel

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,33 +11,6 @@ $(document).ready(function () {
     }, {
         offset: '110%'
     });
-
-    $('.wheel-button').wheelmenu({
-        trigger: 'hover',
-        animation: 'fly',
-        animationSpeed: 'fast',
-    });
-
-    // // Initiate twitter
-    // $('.tweet').twittie({
-    //     count: 1,
-    //     hideReplies: true,
-    //     template: '{{tweet}}'
-    // });
-
-    // // Get Themeforest count
-    // $.getJSON('http://marketplace.envato.com/api/edge/user-items-by-site:'+ themeforest_username +'.json', function (data) {
-    //     var count = data['user-items-by-site'][0].items;
-
-    //     $('#themeforest em').html(count);
-    // });
-
-    // // Get GitHub repo count
-    // $.getJSON('https://api.github.com/users/'+ github_username +'/repos?callback=?', function (data) {
-    //     var count = data['data'].length;
-
-    //     $('#github em').html(count);
-    // });
 });
 
 console.log('No need to snoop around, the source code is available at https://github.com/sonnyt/sonnyt.github.io ;)');
